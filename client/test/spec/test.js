ae3542eec8152085fd1bbfdca0a09d55d262c6db
Bruce Crevensten <bruce.crevensten@gmail.com>
2013-07-31 10:17:34 -0400

    adds one passing DOM-based unit test

--- client/test/spec/test.js
+++ client/test/spec/test.js
@@ -1,10 +1,16 @@
 /*global describe, it */
 'use strict';
+
+chai.should();
+
 (function () {
     describe('Give it some context', function () {
         describe('maybe a bit more context here', function () {
             it('should run here few assertions', function () {
-
+            	var appModel = new client.Models.ApplicationModel();
+		var appView = new client.Views.ApplicationView({el: $('#sandbox'), model: appModel});
+		appView.render();
+		$('#sandbox h1').text().should.equal('Sea Ice Atlas');
             });
         });
     });
