d639297b33d1183cf2c03f132bfc4e29904d94b3
Bruce Crevensten <bruce.crevensten@gmail.com>
2013-07-31 20:19:12 -0400

    verified dist/ works on remote server

--- client/test/spec/test.js
+++ client/test/spec/test.js
@@ -1,17 +1,17 @@
-/*global describe, it */
+/*global describe, it, chai, client */
 'use strict';
 
 chai.should();
 
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-            	var appModel = new client.Models.ApplicationModel();
-		var appView = new client.Views.ApplicationView({el: $('#sandbox'), model: appModel});
-		appView.render();
-		$('#sandbox h1').text().should.equal('Sea Ice Atlas');
-            });
-        });
-    });
+	describe('Give it some context', function () {
+		describe('maybe a bit more context here', function () {
+			it('should run here few assertions', function () {
+				var appModel = new client.Models.ApplicationModel();
+				var appView = new client.Views.ApplicationView({el: $('#sandbox'), model: appModel});
+				appView.render();
+				$('#sandbox h1').text().should.equal('Sea Ice Atlas');
+			});
+		});
+	});
 })();
