e8f129b9c56a9d498fa5b2b426aeb5a0df93b406
Bruce Crevensten <bruce.crevensten@gmail.com>
2013-08-20 13:04:40 -0800

    adds scaffolding for remaining tests

---
+++ client/test/spec/app.js
@@ -0,0 +1,23 @@
+/*global describe, it, chai, client */
+'use strict';
+
+chai.should();
+
+(function () {
+	describe('App router', function () {
+		it('loads the most recent available data by default', function () {
+			var appModel = new client.Models.ApplicationModel();
+		
+		});
+
+		it('checks if the date data is invalid, falling back to most current available', function() {
+
+		});
+
+		it('loads data from the info specified in the URL if present', function() {
+
+		});
+
+		
+	});
+})();
--- client/test/spec/map.js
+++ client/test/spec/map.js
@@ -4,10 +4,34 @@
 chai.should();
 
 (function () {
+	describe('Map view', function() {
+		it('calls its render() method when the dates in the model change', function() {
+
+		});
+
+		// Idea here is to include a config thing for production vs. testing URLs
+		it('composes the layer on the map by combining configuration + info from the model', function() {
+
+		});
+	});
+
 	describe('Map model', function() {
-		it('exists', function() {
-			var mapModel = new client.Models.MapModel();
-			mapModel.should.exist();
+		it('can validate if its year/month/day combo is valid', function() {
+
+		});
+
+		it('can construct the layer name to be used on the server', function() {
+
+		});
+	});
+
+	describe('Map date picker', function() {
+		it('only lists valid date combinations to the user', function() {
+
+		});
+
+		it('has a year, month, and date component', function() {
+
 		});
 	});
 })();
