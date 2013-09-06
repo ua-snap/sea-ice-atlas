8b658c33d51a54413479353968b7c38ff8caac59
Craig Stephenson <craig@hermes.snap.uaf.edu>
2013-09-06 12:59:23 -0800

    Separate concentration and source files are now created. Also have a basic regular expression file listing example in place for later.

--- etc/Bill_sic_nc_to_GTiff.r
+++ etc/Bill_sic_nc_to_GTiff.r
@@ -7,31 +7,15 @@ library(raster)
 library(parallel)
 library(ncdf)
 library(rgdal)
-
-# set an output directory
-outDir = '/tmp/seaiceatlas'
-
-# set a working directory
-setwd('.')
-
-# read in the NetCDF file from Bill as a RasterBrick()
-b <- brick('~/Downloads/alaska.seaice.allweeks.nc')
-
-# make the sequence of dates to identify the layers in the new RasterBrick() and pass those new identifiers to the names()
-dates=seq(as.Date('1953/1/1'), as.Date('2012/12/31'), length.out=nlayers(b))
-names(b) <- gsub('-','_',as.character(dates))
-
-# write out the rasters
-writeRaster(b, format='GTiff', filename=paste(outDir,'sic_mean_pct_weekly_ak',sep="/"), datatype='INT1S',options='COMPRESS=LZW', bylayer=T, suffix=substring(names(b),2,nchar(names(b))))
-
-#########################################################################################################################################################################################################
+library(stringr)
+library(RPostgreSQL)
 
 # a function that does the same thing as above, only funtionally
-seaIceConvert <- function(ncFilePath, outDirPath, outFormat, beginDate, endDate, outPrefix){
+seaIceConvert <- function(ncFilePath, outDirPath, outFormat, beginDate, endDate, outPrefix, varName){
 	if('package:raster' %in% search() == FALSE) require(raster)
 
 	# read in the NetCDF file from Bill as a RasterBrick()
-	b <- brick(ncFilePath)
+	b <- brick(ncFilePath, varname=varName)
 
 	# make the sequence of dates to identify the layers in the new RasterBrick() and pass those new identifiers to the names()
 	dates=seq(as.Date(beginDate), as.Date(endDate), length.out=nlayers(b))
@@ -41,9 +25,73 @@ seaIceConvert <- function(ncFilePath, outDirPath, outFormat, beginDate, endDate,
 	writeRaster(b, format=outFormat, filename=paste(outDirPath,outPrefix,sep="/"), datatype='INT1S',options='COMPRESS=LZW', bylayer=T, suffix=substring(names(b),2,nchar(names(b))))
 }
 
-#########################################################################################################################################################################################################
+ncFilePath = '~/alaska.seaice.allweeks.nc'
+outDirPath = '~/output'
+outFormat =  'GTiff'
+beginDate = '1953/1/1'
+endDate = '2012/12/31'
 
-# an example of how to call the function if that is an easier way to convert it
-# seaIceConvert(ncFilePath='/workspace/UA/malindgren/projects/Tracy_SeaIce/weekly_from_bill/Weekly_SeaIce_Bill_061113/alaska.seaice.quarter.by.quarter.nc', 
-	outDirPath='/workspace/UA/malindgren/projects/Tracy_SeaIce/weekly_from_bill/test', outFormat='GTiff',
-	beginDate='1953/1/1', endDate='2012/12/31', outPrefix='sic_mean_pct_weekly_ak')
+# we're going to create separate files for each of the two variables, for each date
+varNames <- c('seaice_conc', 'seaice_source')
+
+# set a working directory
+setwd('.')
+
+# create separate GeoTIFFs for sea ice concentration and sea ice source data
+for(varName in varNames) {
+	outPrefix = str_c(varName, '_sic_mean_pct_weekly_ak')
+	seaIceConvert(ncFilePath, outDirPath, outFormat, beginDate, endDate, outPrefix, varName)
+}
+
+# connect to PostgreSQL to populate PostGIS raster table
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, host="hermes", user="sea_ice_atlas_user", password="DsUmcsjh", dbname="sea_ice_atlas")
+
+# get a list of all the concentration GeoTIFFs that were created by the writeRaster() function
+outputTiffs <-list.files(path=outDirPath, pattern='^seaice_conc.*\\.tif$') 
+
+for(outputTiff in outputTiffs) {
+
+	# get the date from the GeoTIFF filename
+	pattern = '.*(\\d{4})_(\\d{2})_(\\d{2}).*'
+	dateParts <- str_match(outputTiff, pattern)
+	formattedDate = paste(dateParts[2], dateParts[3], dateParts[4], sep='-')
+	filePath = paste(outDirPath, dateParts[1], sep='/')
+
+	# translate GeoTIFF into EPSG:3338
+	syscall = paste("gdal_translate -of Gtiff -co tfw=yes -a_ullr -180 80.375 -120 40.125 -a_srs EPSG:3338", filePath, "~/tmp/concentration_reproj.tif", sep=" ")
+	system(syscall)
+
+	# convert this GeoTIFF into a PostGIS SQL script
+	syscall = '/usr/pgsql-9.2/bin/raster2pgsql -r ~/tmp/concentration_reproj.tif -s 3338 -I > ~/tmp/concentration_reproj.sql'
+	system(syscall)
+
+	# read the contents of this SQL script
+	contents <- readLines('~/tmp/concentration_reproj.sql')
+	sqlQuery <- paste(contents, collapse="\n")
+
+	# pull raster data out of the SQL script and throw away the rest
+	pattern = 'VALUES \\(\'(.*?)\''
+	queryParts <- str_match(sqlQuery, pattern)
+	rasterData = queryParts[2]
+	insertStatement = paste('INSERT INTO "test" ("date", "rast") VALUES (\'', formattedDate, '\',\'', rasterData, '\'::raster)', sep='')
+	dbGetQuery(con, insertStatement)
+}
+
+# get a list of all the source GeoTIFFs that were created by the writeRaster() function
+outputTiffs <-list.files(path=outDirPath, pattern='^seaice_source.*\\.tif$') 
+
+# to do: add the data source rasters as secondary bands
+for(outputTiff in outputTiffs) {
+}
+
+# the raster2pgsql command did this, so we're adding it back in
+indexStatement = 'CREATE INDEX "test_rast_gist" ON "test" USING gist (st_convexhull("rast"))'
+dbGetQuery(con, indexStatement)
+
+# the raster2pgsql command did this, so we're adding it back in
+analyzeStatement = 'ANALYZE "test"'
+dbGetQuery(con, analyzeStatement)
+
+dbDisconnect(con)
+dbUnloadDriver(drv)
