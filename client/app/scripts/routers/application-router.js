857330f4493bdb14b3a1b74c1da64684901ca849
Craig Stephenson <cstephen@murre.iarc.uaf.edu>
2013-09-25 14:14:25 -0800

    Integrated HighCharts/JSON stuff into web app. Very crudely so far.

--- client/app/scripts/routers/application-router.js
+++ client/app/scripts/routers/application-router.js
@@ -8,10 +8,13 @@ client.Routers.ApplicationRouter = Backbone.Router.extend({
 
 	index:  function() {
 		var appModel = new client.Models.ApplicationModel();
-		var appView = new client.Views.ApplicationView({el: $('#app'), model: appModel});
 
+		var appView = new client.Views.ApplicationView({el: $('#app'), model: appModel});
 		var mapView = new client.Views.MapView();
+		var chartView = new client.Views.ChartView();
+
 		appView.render();
 		mapView.render();
+		chartView.render();
 	}
 });
