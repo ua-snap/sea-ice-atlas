6bd90c07b430aca7e0fb328225d427b1bc5a359c
Bruce Crevensten <bruce.crevensten@gmail.com>
2013-12-05 13:37:20 -0900

    fixes #142, restore state from URL

--- src/scripts/routers/Application.js
+++ src/scripts/routers/Application.js
@@ -125,8 +125,10 @@ client.Routers.ApplicationRouter = Backbone.Router.extend({
 			lon: lon,
 			concentration: concentration
 		}, {silent:true}); // silent because otherwise it triggers a change event, unwanted here.
-		this.renderMap();
-		this.mapModel.trigger('change:lat'); // Force render to reveal charts
+		this.renderMap().then(_.bind(function() {
+			this.mapModel.trigger('change:lat'); // Force render to reveal charts
+		}, this));
+		
 	},
 
 	// User arrived via bookmark
@@ -136,7 +138,9 @@ client.Routers.ApplicationRouter = Backbone.Router.extend({
 			year: year,
 			month: month
 		}, {silent:true}); // silent because otherwise it triggers a change event, unwanted here.
-		this.renderMap();
+		this.renderMap().then(_bind(function() {
+			this.mapModel.trigger('change:month');
+		}, this));
 	},
 
 	checkIfRenderLayout: function() {
@@ -147,6 +151,8 @@ client.Routers.ApplicationRouter = Backbone.Router.extend({
 	},
 
 	renderMap: function() {
+
+		var mapRendered = Q.defer();
 
 		// mapView.render() returns a promise on initial run.  TODO: this may break if base map has already 
 		// rendered, fix/defend.
@@ -175,8 +181,11 @@ client.Routers.ApplicationRouter = Backbone.Router.extend({
 			this.setMapMode('map');
 			this.mapView.loadLayer(this.mapModel.get('year'), this.mapModel.get('month'));
 
+			mapRendered.resolve();
+
 		}, this));
 
+		return mapRendered.promise;
 	},
 
 	// Flag to indicate if the main app layout has rendered or not
