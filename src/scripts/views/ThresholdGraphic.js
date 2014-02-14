6397411bb7aead28c8b921f4c5eb088af18b0b96
Craig Stephenson <cstephen@murre.iarc.uaf.edu>
2014-02-14 12:49:46 -0900

    Open water chart now scales better on the smallest size that renders successfully.

--- src/scripts/views/ThresholdGraphic.js
+++ src/scripts/views/ThresholdGraphic.js
@@ -95,24 +95,27 @@ drawGraphic: function() {
 	tasks.sort(function(a, b) {
 	    return a.endDate - b.endDate;
 	});
+	
 	var maxDate = tasks[tasks.length - 1].endDate;
 	tasks.sort(function(a, b) {
 	    return a.startDate - b.startDate;
 	});
+	
 	var minDate = tasks[0].startDate;
-
 	var format = "%b";
-
-	if($('#thresholdTarget').width() >= 768) {
-		var size = {
-			width: $('#thresholdTarget').width() - 40,
-			height: Math.round($('#thresholdTarget').width() * 0.69)
-		}
+	var scaleFactor;
+	
+	if($('#thresholdTarget').width() < 580) {
+		scaleFactor = 0.38;
+	} else if ($('#thresholdTarget').width() < 768) {
+		scaleFactor = 0.59;
 	} else {
-		var size = {
-			width: $('#thresholdTarget').width() - 40,
-			height: Math.round($('#thresholdTarget').width() * 0.55)
-		}
+		scaleFactor = 0.69;
+	}
+
+	var size = {
+		width: $('#thresholdTarget').width() - 40,
+		height: Math.round($('#thresholdTarget').width() * scaleFactor)
 	}
 
 	// To prevent a page scroll jump upon resizing and/or updating the chart
