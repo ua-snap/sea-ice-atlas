8f05d27e806811364b4b7581ba8d06826555afd0
EarthScientist <lindgren.mike@gmail.com>
2014-08-29 14:53:58 -0800

    added in processing of the monthly data from Bill

--- etc/hsia_nc_convert_validate.py
+++ etc/hsia_nc_convert_validate.py
@@ -38,35 +38,10 @@
 from scipy.io import netcdf
 import numpy as np
 import rasterio, os
+from itertools import izip
 
 os.chdir( '/workspace/Shared/Tech_Projects/Sea_Ice_Atlas/project_data' )
 
-# source NetCDF file, found here (http://igloo.atmos.uiuc.edu/SNAP/alaska.observed.seaice.weekly.nc)
-# DAILIES (JULY 2014)
-# nc_path = './Outputs_From_Bill/SIC_Weekly/netcdf/version2_from_bill/alaska.observed.seaice.weekly.nc'
-# monthlies (AUGUST 2014)
-nc_path = './Outputs_From_Bill/Bill_monthly_outputs_Aug2014/alaska.monthly.ice.concentration.sources.1850-2013.nc'
-# read in the data using scipy
-nc = netcdf.netcdf_file( nc_path )
-
-# set the  timestep for the output filename as a string
-timestep = 'monthly' # 'weekly'
-
-# set some output filename constants to pass to the generator
-output_path = './Outputs_From_Bill/Bill_monthly_outputs_Aug2014'
-output_prefix = 'sic_mean_pct_' + timestep + '_ak_' 
-
-# we know the variable names are 'seaice_conc' and 'seaice_source'
-conc = nc.variables[ 'seaice_conc' ]
-source = nc.variables[ 'seaice_source' ]
-
-# we need to do something with the metadata of the output new GTiffs here
-# --> for the time-being I am just reading in an old version of the GTiff to use as a template
-template = rasterio.open( './CODE/sea-ice-atlas/etc/raster_template_Bill.tif' ) 
-meta = template.meta
-meta.update( compress='lzw', nodata=-1, dtype=rasterio.float32 ) # -1 nodata is land in this map
-
-# some functions to be used in the script
 def arr2d_to_gtiff( arr_2d, output_filename, template_meta ):
 	"""
 	simple function to convert a numpy 2d_array to a GTiff, or if there
@@ -102,49 +77,113 @@ def arr2d_to_gtiff( arr_2d, output_filename, template_meta ):
 	gtiff.close()
 	return gtiff.name
 
+def zip_sea_ice( in_dir, output_filename ):
+	'''
+	zip up the sea ice data.
 
-# Goal here is to generate filenames.
-# For the version of the data with MD5 sum 845d2ee0953fcd5f8e977d0cfb023f3d,
-# the structure of this data has metadata in records 0-5 and the last record (2935) is the overall filename.
-names = nc.history.split()
-names = names[ 6:len(names)-1 ]
-dates = [ i.split('.')[len(i.split('.'))-2] for i in names ]
+	arguments:
+	in_dir = directory where the output GTiffs are stored
+	output_filename = string filename of the new zip file to be 
+		created by the function.
 
-# generator for the list comprehension to do the conversion
-input_generator = ( ( os.path.join( output_path, timestep, output_prefix + '_'.join([ d[4:6], d[6:8], d[:4] ]) + '.tif' ), c.astype(meta['dtype']), s.astype(meta['dtype']) ) \
-						for d, c, s in zip( dates, conc, source ) )
+	depends:
+		python standard library
+	'''
 
-# run it with a list comprehension
-gtiff_out = [ arr2d_to_gtiff( (cnc, src), fn, meta ) for fn, cnc, src in input_generator ]
+	import zipfile, os, glob
+	compression = zipfile.ZIP_DEFLATED
+	os.chdir( in_dir )
+	files = glob.glob( os.path.join( in_dir, '*.tif' ) )
+	zf = zipfile.ZipFile( output_filename, mode='w' )
+	for i,f in enumerate(files):
+		zf.write( f, arcname=os.path.basename( f ), compress_type=compression )
+	zf.close()
+	return zf
 
-# # SOURCE TEST
-# what are the unique acceptable values allowed in the seaice_source layer?
-value_list = [ -1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21 ]
 
-# re-instantiate the generator
-input_generator = ( ( os.path.join( output_path, 'weekly', output_prefix + '_'.join([ d[4:6], d[6:8], d[:4] ]) + '.tif' ), c.astype(meta['dtype']), s.astype(meta['dtype']) ) \
-						for d, c, s in zip( dates, conc, source ) )
+input_path = '/workspace/Shared/Tech_Projects/Sea_Ice_Atlas/project_data/HSIA_SIC_Chapman'
+version_num = 'v1_0'
 
-# get all of the unique values for each source layer as a dict
-out = {num:np.unique(src).tolist() for num, src in enumerate(source) }
-all_vals = [j for i in out.values() for j in i ]
-all_unique_vals = np.unique(np.array(all_unique_vals))
+# current source NetCDF files found here:
+#	DAILIES (JULY 2014) (http://igloo.atmos.uiuc.edu/SNAP/alaska.observed.seaice.weekly.nc) MD5 sum 845d2ee0953fcd5f8e977d0cfb023f3d
+#	MONTHLIES (AUGUST 2014) (http://www.atmos.uiuc.edu/~wlchapma/alaska.monthly.ice.concentration.sources.1850-2013.nc)	MD5 sum 6ac0e57ed1c0d6da8308ceeaec9c6700
+nc_paths = [os.path.join( input_path, version_num, 'weekly', 'netcdf', 'alaska.observed.seaice.weekly.nc'),\
+			os.path.join( input_path, version_num, 'monthly', 'netcdf', 'alaska.monthly.ice.concentration.sources.1850-2013.nc')]
 
-# if the output of the next line is True then there is no discrepancy between the unique source ids and what we should expect.
-False not in [ True for i in all_unique_vals if i in value_list ] 
+# timesteps
+timesteps = ['monthly', 'weekly']
 
-# # CONC TEST
-# re-instantiate the generator
-input_generator = ( ( os.path.join( output_path, 'weekly', output_prefix + '_'.join([ d[4:6], d[6:8], d[:4] ]) + '.tif' ), c.astype(meta['dtype']), s.astype(meta['dtype']) ) \
-						for d, c, s in zip( dates, conc, source ) )
-# are the values of the dataset seaice_conc only within the range of 0-100?
-cnc_range_list = [ (np.min(cnc), np.max(cnc)) for fn, cnc, src in input_generator ]
-# now write a test to be sure the range is right
-cnc_test_list = [ 1 for min, max in cnc_range_list if min >= 0 and max <= 1 ]
-# diffenence between the length input list and the sum of the cnc_test_list
-cnc_final_test = len( cnc_range_list ) - sum( cnc_test_list )
+for timestep, nc_path in izip( timesteps, nc_paths ):
+	print nc_path
+	# read in the data using scipy
+	nc = netcdf.netcdf_file( nc_path )
+	# set some output filename constants to pass to the generator
+	# output_path = './Outputs_From_Bill/Bill_monthly_outputs_Aug2014'
+	output_path = os.path.join( input_path, version_num, timestep, 'gtiff' )
+	output_prefix = 'sic_mean_pct_' + timestep + '_ak_' 
 
-# old code for creating monthlies from weeklies before Bill sent the Monthly outputs in August 2014
-# # # GRAB AND COPY "MONTHLY" TIMESERIES 
-# # grab the week of the 15th of the month for the "Monthly" dataset
-# [ os.system( 'cp ' + fn + ' ' + fn.replace('weekly', 'monthly') ) for fn in gtiff_out if '_15_' is in fn ]
+	# we know the variable names are 'seaice_conc' and 'seaice_source'
+	conc = nc.variables[ 'seaice_conc' ]
+	source = nc.variables[ 'seaice_source' ]
+
+	# we need to do something with the metadata of the output new GTiffs here
+	# --> for the time-being I am just reading in an old version of the GTiff to use as a template
+	template = rasterio.open( os.path.join( input_path, 'CODE', 'sea-ice-atlas', 'etc', 'raster_template_Bill.tif' ) 
+	meta = template.meta
+	meta.update( compress='lzw', nodata=-1, dtype=rasterio.float32 ) # -1 nodata is land in this map
+
+	# Goal here is to generate filenames.
+	# For the version of the data with MD5 sum 845d2ee0953fcd5f8e977d0cfb023f3d,
+	# the structure of this data has metadata in records 0-5 and the last record (2935) is the overall filename.
+	names = nc.history.split()
+	names = names[ 6:len(names)-1 ]
+	dates = [ i.split('.')[len(i.split('.'))-2] for i in names ]
+
+	# generator for the list comprehension to do the conversion
+	input_generator = ( ( os.path.join( output_path, output_prefix + '_'.join([ d[4:6], d[6:8], d[:4] ]) + '.tif' ), c.astype(meta['dtype']), s.astype(meta['dtype']) ) \
+							for d, c, s in izip( dates, conc, source ) )
+
+	# run it with a list comprehension
+	gtiff_out = [ arr2d_to_gtiff( (cnc, src), fn, meta ) for fn, cnc, src in input_generator ]
+
+	# run the zipping procedure
+	in_dir = os.path.join( input_path, version_num, timestep )
+	output_filename = os.path.join( in_dir, 'sic_hsia_' + timestep + '_'+dates[0]+'_'+dates[len(dates)-1]+'.zip' )
+
+	if os.path.exists( output_filename ):
+		os.remove( output_filename )
+
+	out = zip_sea_ice( in_dir, output_filename )
+	out.close()
+
+	# # # # # # # # # # # # # # # # # # # # # #
+	# BELOW ARE SOME TESTS THAT CAN BE RUN ON THE DATA 
+	# # SOURCE TEST
+	# what are the unique acceptable values allowed in the seaice_source layer?
+	value_list = [ -1,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21 ]
+
+	# re-instantiate the generator
+	input_generator = ( ( os.path.join( output_path, 'weekly', output_prefix + '_'.join([ d[4:6], d[6:8], d[:4] ]) + '.tif' ), c.astype(meta['dtype']), s.astype(meta['dtype']) ) \
+							for d, c, s in zip( dates, conc, source ) )
+
+	# get all of the unique values for each source layer as a dict
+	out = {num:np.unique(src).tolist() for num, src in enumerate(source) }
+	all_vals = [j for i in out.values() for j in i ]
+	all_unique_vals = np.unique(np.array(all_vals))
+
+	# if the output of the next line is True then there is no discrepancy between the unique source ids and what we should expect.
+	False not in [ True for i in all_unique_vals if i in value_list ] 
+
+	# # CONC TEST
+	# re-instantiate the generator
+	input_generator = ( ( os.path.join( output_path, 'weekly', output_prefix + '_'.join([ d[4:6], d[6:8], d[:4] ]) + '.tif' ), c.astype(meta['dtype']), s.astype(meta['dtype']) ) \
+							for d, c, s in zip( dates, conc, source ) )
+	# are the values of the dataset seaice_conc only within the range of 0-100?
+	cnc_range_list = [ (np.min(cnc), np.max(cnc)) for fn, cnc, src in input_generator ]
+	# now write a test to be sure the range is right
+	cnc_test_list = [ 1 for min, max in cnc_range_list if min >= 0 and max <= 1 ]
+	# diffenence between the length input list and the sum of the cnc_test_list
+	cnc_final_test = len( cnc_range_list ) - sum( cnc_test_list )
+
+
+
