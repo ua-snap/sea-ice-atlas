f390a6b7bacacd8a049b66b1c286386ff35ea449
Craig Stephenson <cstephen@murre.iarc.uaf.edu>
2014-12-01 12:40:43 -0900

    Changed path in seedTilecache.sh script to reflect current environment on Hades.

--- etc/seedTilecache.sh
+++ etc/seedTilecache.sh
@@ -3,7 +3,7 @@
 # this script should be run in the /var/www/tilecache directory on Hades;
 # it'll force population around zoom level 4 of all sea ice atlas tiles in the cache.
 
-for f in /var/www/html/seaice-reprojected-weekly/*
+for f in /var/www/html/seaice-monthly/*
 do
 	# Strip trailing file extension and leading file paths:
 	y=${f%.tif}
